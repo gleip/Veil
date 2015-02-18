c70efba08f037704288ed7937de47873a4628a56
gleip <3on.gleip@gmail.com>
2015-02-18 14:20:24 +0400

    Add function ParseHtmlBlock job_18022015

--- veil.js
+++ veil.js
@@ -15,12 +15,68 @@ $(document).ready(function() {
                 var veilBlock = $(content, article);
                 if(veilBlock.length) {
                     for(var i = 0; veilBlock.length > i; i++) {
-                        if($(veilBlock[i]).html().length > characters) {
-                            veilBlock(11111);
+                        if($(veilBlock[i]).text().length > characters) {
+                            veilGo($(veilBlock[i]));
                         }
                     }
                 }
             }
+
+            function veilGo(veilBlock) {
+                var textBlock = veilBlock.text();
+                var htmlBlock = veilBlock.html();
+                parseHtmlBlock(htmlBlock);
+            }
+
+            function parseHtmlBlock(htmlBlock) {
+                var htmlArray = [];
+                var textArray = [];
+                var start = 0;
+                for(var count = 0; htmlBlock.length > count; count++) {
+                    if(htmlBlock.slice(count, count+1) === '<') {
+                        start = count;
+                    }
+                    if(htmlBlock.slice(count, count+1) === '>') {
+                        htmlArray.push(htmlBlock.slice(start, count+1));
+                    }
+                }
+                console.log(htmlArray);
+            }
+
+            //function getWord(textBlock) {
+            //    var count = characters+1;
+            //    var start = characters;
+            //    var stop, word;
+            //    while(textBlock.slice(start, count) != ' ') {
+            //        start++;
+            //        count++;
+            //    }
+            //    stop = count;
+            //    count = characters;
+            //    start = characters-1;
+            //    while(textBlock.slice(start, count) != ' ') {
+            //        start--;
+            //        count--;
+            //    }
+            //    word = textBlock.slice(start, stop);
+            //
+            //    return word;
+            //}
+
+            //function setPointer(textBlock) {
+            //    var count = characters+1;
+            //    var start = characters;
+            //    var pointer, text;
+            //    while(textBlock.slice(start, count) != ' ') {
+            //        start++;
+            //        count++;
+            //    }
+            //    pointer = count;
+            //
+            //    return textBlock.slice(0,pointer) + ' <VeilPointer> ' + textBlock.slice(pointer);
+            //
+            //}
+
         }
 
     })();
